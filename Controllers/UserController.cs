c8b1f06bcc6baa649e7dd036cddfcaf67e88b892
FELIPE CAMARGO DE ALMEIDA <felipe.almeida@cs.fsg.edu.br>
2025-02-26 21:26:30 -0300

    Created BaseController and TestMethod for implementation. Started the implementation of UserController.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SystemImports.Api.Controllers.Base;
+
+namespace SystemImports.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> ValidateUserAsync()
+        {
+            return await ExecuteTaskAsync(TestTaskMethod);
+        }
+
+        private async Task<string> TestTaskMethod()
+        {
+            return "Task Executed";
+        }
+    }
+}
